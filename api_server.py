4ec7c92eef73cedbdb89b1c4d08176648f58895c
kuwahara <ee68028@yahoo.co.jp>
2024-11-06 21:31:21 +0900

    Bug Fixed

--- api_server.py
+++ api_server.py
@@ -8,13 +8,18 @@ app = Flask(__name__)
 def run_future():
     # リクエストからパラメータを取得
     data = request.get_json()
+    print(data)
     tickers = data.get('tickers')
-    is_output_all_info = data.get('flag', False)
-    is_send_line = data.get('flag', True)
+    tickers = [ticker.strip().upper() for ticker in tickers.split(',')] 
+    print(tickers)
+    is_output_all_info = data.get('is_output_all_info', False)
+    is_send_line = data.get('is_send_line', True)
 
+    # return jsonify({"error": str(e)}), 500
     # main.py内のfuture関数を呼び出し
     try:
         result = MainPy.main(tickers, is_output_all_info = is_output_all_info, is_send_line = is_send_line)
+        # return jsonify({"result": 'result'}), 200
         return jsonify({"result": result}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
